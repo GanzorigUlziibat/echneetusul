c2d212443095d62742e3fe6464c781659f949975
Odmaral <lnodmaral@gmail.com>
2025-04-27 00:15:54 +0800

    app

---
+++ Odmaral_travelApp/components/CategoryButtons.tsx
@@ -0,0 +1,106 @@
+import {
+  StyleSheet,
+  View,
+  Text,
+  Touchable,
+  TouchableOpacity,
+} from "react-native";
+import React, { useRef, useState } from "react";
+import Colors from "@/constants/Colors";
+import { ScrollView } from "react-native-gesture-handler";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import destinationCategories from "@/data/categories";
+
+const CategoryButtons = () => {
+  const itemRef = useRef<TouchableOpacity[] | null[]>([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const handlerSelectCategory = (index: number) => {
+    setActiveIndex(index);
+    console.log(index);
+  };
+
+  return (
+    <View>
+      <Text style={styles.title}>Categories</Text>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          gap: 20,
+          paddingVertical: 10,
+          marginBottom: 10,
+        }}
+      >
+        {destinationCategories.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            ref={(el) => itemRef.current[index] == el}
+            onPress={() => handlerSelectCategory(index)}
+            style={
+              activeIndex == index
+                ? styles.categoryBtnActive
+                : styles.categoryBtn
+            }
+          >
+            <MaterialCommunityIcons
+              name={item.iconName as any}
+              size={20}
+              color={activeIndex == index ? Colors.white : Colors.black}
+            />
+            <Text
+              style={
+                activeIndex == index
+                  ? styles.categoryBtnTxtActive
+                  : styles.categoryBtnTxt
+              }
+            >
+              {item.title}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default CategoryButtons;
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 22,
+    fontWeight: "700",
+    color: Colors.black,
+  },
+  categoryBtn: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.white,
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    borderRadius: 10,
+    shadowColor: "#333333",
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+  },
+  categoryBtnActive: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.primaryColor,
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    borderRadius: 10,
+    shadowColor: "#333333",
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+  },
+  categoryBtnTxt: {
+    marginLeft: 5,
+    color: Colors.black,
+  },
+  categoryBtnTxtActive: {
+    marginLeft: 5,
+    color: Colors.white,
+  },
+});
