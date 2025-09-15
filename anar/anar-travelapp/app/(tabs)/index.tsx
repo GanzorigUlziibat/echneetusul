4d176527e61afe8a15e56479e9618f7253e26e22
Anar <anar.tuvshinjargal@gmail.com>
2025-09-16 00:04:11 +0800

    13:51

--- anar/anar-travelapp/app/(tabs)/index.tsx
+++ anar/anar-travelapp/app/(tabs)/index.tsx
@@ -1,6 +1,11 @@
 import { StyleSheet, Text, TouchableOpacity, Image, View } from 'react-native';
 import React from 'react';
 import { Stack } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+
+
+
+
 
 
 
@@ -10,13 +15,23 @@ const Page = () => {
       headerTransparent: true,
       headerTitle: "",
       headerLeft: () => (
-        <TouchableOpacity onPress={() => {}}>
+        <TouchableOpacity onPress={() => {}} style={{marginLeft: 20}}>
           <Image 
             source={{
               uri: "https://reactnative.dev/img/tiny_logo.png"
             }}
             style={{width:40, height: 40, borderRadius:10}}
           />
+        </TouchableOpacity>
+      ),
+      headerRight: () => (
+        <TouchableOpacity onPress={() =>{}} style={{
+          marginRight: 20,
+          backgroundColor: "black",
+          padding: 10,
+          borderRadius: 10,
+        }}>
+          <Ionicons name="notifications" size={20} color="black" />
         </TouchableOpacity>
       ),
     }} />
