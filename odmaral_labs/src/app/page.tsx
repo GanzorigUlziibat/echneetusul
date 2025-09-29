46fd7d2bf168740870a5888fb3544fa416a22314
Odmaral <lnodmaral@gmail.com>
2025-09-29 15:40:25 +0800

    lab1,2.1

---
+++ odmaral_labs/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World! 🎉</h1>
+      <p>My first Next.js app</p>
+    </div>
+  );
+}
