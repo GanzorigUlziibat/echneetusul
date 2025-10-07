9586f25d2e5b20c9e4cc590563646e6e31ee473c
Odmaral <lnodmaral@gmail.com>
2025-10-07 11:21:46 +0800

    lab4.1 static

--- odmaral_labs/src/app/about/page.tsx
+++ odmaral_labs/src/app/about/page.tsx
@@ -1,47 +1,9 @@
 import React from "react";
-import styles from "./page.module.css";
-
-import Header from "@/component/header";
-
-export default function About() {
-  <div className={styles.container}>
-    <header className={styles.header}>
-      <h1 className={styles.title}>Миний тухай</h1>
-      <p className={styles.subtitle}>Намтар, боловсрол, хобби</p>
-    </header>
-
-    <section className={styles.card}>
-      <h2>Намтар</h2>
-      <div className={styles.grid}>
-        <div>
-          <div className={styles.muted}>Төрсөн он</div>
-          <div className={styles.value}>1996 он</div>
-        </div>
-        <div>
-          <div className={styles.muted}>Төрсөн газар</div>
-          <div className={styles.value}>Улаанбаатар, Монгол</div>
-        </div>
-      </div>
-    </section>
-
-    <section className={styles.card}>
-      <h2>Боловсрол</h2>
-      <ul className={styles.list}>
-        <li>2002–2012: 20-р Сургууль </li>
-        <li>2012–2016: Мандах Бүртгэл Дээд Сургууль – Нягтлан Бодогч</li>
-        <li>2012–2016: Мандах Их Сургууль – Магитср</li>
-      </ul>
-    </section>
-
-    <section className={styles.card}>
-      <h2>Хобби</h2>
-      <div className={styles.badges}>
-        {["Унших", "Аялах", "Хоол", "Кино"].map((h) => (
-          <span key={h} className={styles.badge}>
-            {h}
-          </span>
-        ))}
-      </div>
-    </section>
-  </div>;
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Энэ бол манай тухай мэдээлэл.</p>
+    </div>
+  );
 }
