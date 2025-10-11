4996d657cb9b2de8e8d6e433608fdf37ff257e0f
Enkhmandal <Enkhmandal95@yahoo.com>
2025-10-11 11:37:58 +0800

    My new next app

---
+++ Mandal1/my-app/src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+
+export default function About() {
+  return (
+    <>
+      <Header />
+      <main className="p-6">
+        <h2 className="text-2xl font-bold">Миний тухай</h2>
+        <p>Next.js ашиглаж байна.</p>
+      </main>
+    </>
+  );
+}
