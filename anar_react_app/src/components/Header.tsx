927278ef9e2dd51eb85057e2cd1b8ebbaeb43038
Anar <anar.tuvshinjargal@gmail.com>
2025-10-11 11:15:47 +0800

    Anar Lab 5-4

--- anar_react_app/src/components/Header.tsx
+++ anar_react_app/src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return(
         <header className="p-4 bg-blue-600 text-white">
-            <h1 className="text-xl">Миний веб</h1>
+            <h1 className="text-xl">Миний веб шүү</h1>
         </header>
     )
 }
