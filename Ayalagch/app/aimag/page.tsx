8191febb9b27b5edfaa6e265a51c53ffd83a0f83
Anar <anar.tuvshinjargal@gmail.com>
2025-12-06 10:43:50 +0800

    aimag updated

--- Ayalagch/app/aimag/page.tsx
+++ Ayalagch/app/aimag/page.tsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
-import { View, Text, FlatList, TouchableOpacity, StyleSheet, Image} from "react-native";
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, Modal, Dimensions  } from "react-native";
 import { aimagDB, db } from "@/app/database/aimag";
+import {Image} from "expo-image";
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   const [aimags, setAimags] = useState<any[]>([]);
+  const [modalVisible, setModalVisible] = useState(false);
+   const [aimagwiki, setAimagwiki] = useState("");
+   const [aimagname, setAimagname] = useState("");
 
   useEffect(() => {
     aimagDB();
     const rows = getAimags();
     setAimags(rows);
+    console.log(aimags);
   }, []);
 
 
@@ -17,6 +23,17 @@ export default function App() {
     return db.getAllSync("SELECT * FROM aimag");
   };
 
+      const handleOpenModal = (wiki, aname) => {
+      setModalVisible(true);
+      setAimagwiki(wiki);
+      setAimagname(aname);
+    };
+
+
+    const handleCloseModal = () => {
+      setModalVisible(false);
+    };
+
 
   return (
     <View style={{ padding: 20 }}>
@@ -24,7 +41,7 @@ export default function App() {
         Аймгийн жагсаалт
       </Text>
 
-       <Image style={{ width: 50 }}
+       <Image style={{ width: '100%', aspectRatio: 2 }}
         source={ require('@/assets/zurag/gazriinzurag1050x510.png')}  
       />
 
@@ -32,6 +49,7 @@ export default function App() {
         data={aimags}
         keyExtractor={(item) => item.aid.toString()}
         renderItem={({ item }) => (
+          <TouchableOpacity  onPress={() => handleOpenModal(item.awiki, item.aname)}>
           <View
             style={{
               padding: 12,
@@ -45,8 +63,80 @@ export default function App() {
             </Text>
             <Text>{item.awiki}</Text>
           </View>
+          </TouchableOpacity>
         )}
       />
+
+         <Modal
+      animationType="slide" // or "fade", "none"
+      transparent={true} // Set to true for an overlay effect
+      visible={modalVisible}
+      onRequestClose={() => setModalVisible(false)} // For Android back button
+    >
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>{aimagname}</Text>
+
+            <WebView style={{width:Dimensions.get('window').width - 50, height:350}}
+      
+      source={{ uri: aimagwiki }} // Replace with your target website
+    />
+          <TouchableOpacity
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => handleCloseModal()}
+          >
+            <Text style={styles.textStyle}>Hide Modal</Text>
+
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
     </View>
   );
 }
+
+
+    const styles = StyleSheet.create({
+      centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+  
+      },
+      modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        width: Dimensions.get('window').width - 30,
+        height:  Dimensions.get('window').height - 100,
+
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      },
+      button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+      },
+      buttonClose: {
+        backgroundColor: '#2196F3',
+      },
+      textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+      modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+      },
+    });
